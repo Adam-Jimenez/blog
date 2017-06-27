c7f04c1043cb2aa51af2c61cf9b71673a1ece9fd
Adam Jimenez <jimenez.adam0@gmail.com>
2017-06-26 23:13:12 -0400

    https support

--- src/config/production.js
+++ src/config/production.js
@@ -1,4 +1,4 @@
 module.exports = {
     env: 'production',
-    backendUrl: 'http://ec2-13-59-235-71.us-east-2.compute.amazonaws.com:3000/'
+    backendUrl: 'https://ec2-13-59-235-71.us-east-2.compute.amazonaws.com:3000/'
 }
